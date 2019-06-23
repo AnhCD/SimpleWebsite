7d2930398d0281a513e5b73ffa657834007e449f
Cong Anh <ohodavi1@gmail.com>
2019-06-23 15:38:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/w3css/3/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 <nav class="w3-bar w3-black">
   <a href="#home" class="w3-button w3-bar-item">Home</a>
   <a href="#band" class="w3-button w3-bar-item">Band</a>
