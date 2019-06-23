5fb22992fc2c9446dfbf43d22d0f5ffaab7f6582
Cong Anh <ohodavi1@gmail.com>
2019-06-23 16:31:21 +0700

    add style

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
 <body>
 <!-- Navbar -->
 <div class="w3-top">
