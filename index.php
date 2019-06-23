a305c23510c3e6d33ef26189e686dd5463cb3ca3
Cong Anh <ohodavi1@gmail.com>
2019-06-23 15:59:15 +0700

    Slide show

--- index.php
+++ index.php
@@ -3,12 +3,25 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/w3css/3/w3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<body>
 <nav class="w3-bar w3-black">
   <a href="ConnectToDB.php" class="w3-button w3-bar-item">View</a>
   <a href="InsertData.php" class="w3-button w3-bar-item">Insert</a>
   <a href="UpdateData.php" class="w3-button w3-bar-item">Update</a>
   <a href="DeleteData.php" class="w3-button w3-bar-item">Delete</a>
 </nav>
+
+<!-- Slide Show-->
+<section>
+  <img class="mySlides" src="rubik_cube.jpeg"
+  style="width:100%">
+  <img class="mySlides" src="red_car.jpg"
+  style="width:100%">
+  <img class="mySlides" src="Lego.jpg"
+  style="width:100%">
+  <img class="mySlides" src="figue.jpg"
+  style="width:100%">
+</section>
 </body>
      
 </html>
