8c9b51cb0d496e05f68948a79b54dc48a9496e1c
Cong Anh <ohodavi1@gmail.com>
2019-06-24 19:04:58 +0700

    change image

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ img {vertical-align: middle;}
 
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:500px" id="home">
-  <img class="w3-image" src="image/hamburger.jpg" alt="Hamburger Catering" width="1600" height="800">
+  <img class="w3-image" src="image/toys.jpg" alt="Toys" width="1600" height="800">
   <div class="w3-display-bottomleft w3-padding-large w3-opacity">
     <h1 class="w3-xxlarge">ATN toys shop</h1>
   </div>
